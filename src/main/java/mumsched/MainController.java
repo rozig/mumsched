482a1b22f037d6a5ae15dd960cbd3744b5db6d19
unknown <986268@CS.MUM.EDU>
2018-01-24 09:59:16 -0600

    :tada: initial commit.

---
+++ src/main/java/mumsched/MainController.java
@@ -0,0 +1,14 @@
+package mumsched;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class MainController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+}
