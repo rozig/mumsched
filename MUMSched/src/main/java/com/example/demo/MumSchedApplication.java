4720d576cda8ea037ab7c0a7f765e4f6a8709c80
ling <lingsun@mum.edu>
2018-01-25 14:22:50 -0600

    test

--- MUMSched/src/main/java/com/example/demo/MumSchedApplication.java
+++ MUMSched/src/main/java/com/example/demo/MumSchedApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MumSchedApplication {
 
-	public static void main(String[] args) {
+ static void main(String[] args) {
+	 	//test
 		SpringApplication.run(MumSchedApplication.class, args);
 	}
 }
