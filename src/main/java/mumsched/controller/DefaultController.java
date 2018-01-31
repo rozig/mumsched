00384aad914b6621fbbeccac459094997d51e919
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-01-31 14:48:44 -0600

    Created entity classes

--- src/main/java/mumsched/controller/DefaultController.java
+++ src/main/java/mumsched/controller/DefaultController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import mumsched.entity.User;
 import mumsched.entity.Role;
--- src/main/java/mumsched/entity/Student.java
+++ src/main/java/mumsched/entity/Student.java
@@ -3,39 +3,87 @@ package mumsched.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
 @Entity
-public class Student {
+public class Student extends Profile {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
-    private String name;
+    private Date birthDate;
 
-    private String email;
+    private Boolean track;
 
-    public Integer getId() {
-        return id;
+    private Boolean ptType;
+
+    private Boolean isUSResident;
+
+    @ManyToMany(
+        fetch=FetchType.LAZY,
+        cascade={
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+        }
+    )
+    @JoinTable(
+        name="section_students",
+        joinColumns={@JoinColumn(name="student_id")},
+        inverseJoinColumns={@JoinColumn(name="section_id")}
+    )
+    private List<Section> registeredSections = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name="entry_id", nullable=false)
+    private Entry entry;
+
+    public Date getBirthDate() {
+        return birthDate;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
 
-    public String getName() {
-        return name;
+    public Boolean getTrack() {
+        return track;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTrack(Boolean track) {
+        this.track = track;
     }
 
-    public String getEmail() {
-        return email;
+    public Boolean getPtType() {
+        return ptType;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPtType(Boolean ptType) {
+        this.ptType = ptType;
+    }
+
+    public Boolean getIsUSResident() {
+        return isUSResident;
+    }
+
+    public void setIsUSResident(Boolean isUSResident) {
+        this.isUSResident = isUSResident;
+    }
+
+    public List<Section> getRegisteredSections() {
+        return registeredSections;
+    }
+
+    public void addRegisteredSection(Section section) {
+        this.registeredSections.add(section);
     }
 }
