c54388042888738d6354b043d20e164e98a564a5
enkuso <eegii2you@yahoo.com>
2018-02-01 14:37:40 -0600

    Course data seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,27 +12,27 @@ INSERT INTO entry(id, name, date, mpp_number, fpp_number, opt_number) VALUES
     Student Seed
  */
 -- INSERT INTO student(name, email) VALUES
---     ("Ganzorig", "gerdenebat@mum.edu"),
---     ("Enkhbayasgalan", "enk@mum.edu");
+--     ('Ganzorig', 'gerdenebat@mum.edu'),
+--     ('Enkhbayasgalan', 'enk@mum.edu');
 
 
 
 /*
     Course Seed
  */
--- INSERT INTO course(id, code, name, description, prereq_course_id) VALUES
---     (1, "CS472", "Web Programming", "WAP", null),
---     (2, "CS525", "Advanced Software Development", "ASD", null),
---     (3, "CS544", " Enterprise Architecture", "EA", null),
---     (4, "CS545", "Web Application Architecture", "WAA", 1),
---     (5, "CS435", "Algorithms", "Algo", null),
---     (6, "CS471", "Parallel Programming", "PP", null),
---     (7, "CS473", "Mobile Device Programming", "MDP", null),
---     (8, "CS522", "Big Data", "BD", null),
---     (9, "CS572", "Modern Web Applications", "MWA", 1),
---     (10, "CS523", "Big Data Technology", "BDT", null),
---     (11, "CS582", "Machine Learning", "ML", null),
---     (12, "CS425", "Software Engineering", "SWE", null);
+INSERT INTO course(id, code, name, level, max_student, prereq_course_id) VALUES
+    (1, 'CS472',  'Web Programming', 400, 25, null),
+    (2, 'CS525',  'Advanced Software Development', 500, 25, null),
+    (3, 'CS544',  'Enterprise Architecture', 500, 25, 1),
+    (4, 'CS545',  'Web Application Architecture', 500, 25, 1),
+    (5, 'CS435',  'Algorithms', 400, 25, null),
+    (6, 'CS471',  'Parallel Programming', 400, 25, null),
+    (7, 'CS473',  'Mobile Device Programming', 400, 25, null),
+    (8, 'CS522',  'Big Data', 500, 25, null),
+    (9, 'CS572',  'Modern Web Applications', 500, 25, 1),
+    (10, 'CS523', 'Big Data Technology', 500, 25, null),
+    (11, 'CS582', 'Machine Learning', 500, 25, null),
+    (12, 'CS425', 'Software Engineering', 400, 25, null);
 
 
 
@@ -49,10 +49,15 @@ INSERT INTO role(role) VALUES
  */
 INSERT INTO user(active, email, last_name, name, password) VALUES
     (1, 'gerdenebat@mum.edu', 'Erdenebat', 'Ganzorig', '$2a$10$8NFDu5a1mhVOiHavtl2cbOPXKTQNw2/9PEwJF1xhZvhyBj4mJrY4O'),
-    (2, 'egalsandorj@mum.edu', 'Galsandorj', 'Enkhbayasgalan', '$2a$10$mL9l.om2APqDfuqIndw9a.XmE67dm/0e5tVMYo9ldAliD9U6CLNa2');
+    (2, 'egalsandorj@mum.edu', 'Galsandorj', 'Enkhbayasgalan', '$2a$10$8NFDu5a1mhVOiHavtl2cbOPXKTQNw2/9PEwJF1xhZvhyBj4mJrY4O'),
+    (3, 'student@mum.edu', 'Doe', 'John', '$2a$10$8NFDu5a1mhVOiHavtl2cbOPXKTQNw2/9PEwJF1xhZvhyBj4mJrY4O'),
+    (4, 'faculty@mum.edu', 'Nolle', 'Steve', '$2a$10$8NFDu5a1mhVOiHavtl2cbOPXKTQNw2/9PEwJF1xhZvhyBj4mJrY4O');
 
 /*
     User-Role Seed
  */
 INSERT INTO user_role(user_id, role_id) VALUES
-    (1, 2);
+    (1, 2),
+    (2, 2),
+    (3, 1),
+    (4, 3);
